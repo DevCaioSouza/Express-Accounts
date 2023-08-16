c45414063d9d2c661b3252f50a3c2a21539d60e0
Caio Camargo <caiocbahia@hotmail.com>
2023-08-16 12:29:25 -0300

    setting prompt for create account - chalk

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ function operation() {
       const action = answer["action"];
 
       console.log(action);
+
+      if (action === "Create an Account") {
+        createAccount();
+      }
     })
     .catch((err) => console.log(err));
+}
+
+function createAccount() {
+  console.log(
+    chalk.bgGreen.black("Congratulations. Thanks for choosing our bank!")
+  );
+  console.log(chalk.green("Fill the following fields with your personal data"));
 }
